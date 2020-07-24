31c7727de7c940df23d5903e1293208e9f6fa911
Rafael Lopes <rafalopes@live.com>
2020-07-24 19:10:52 -0100

    Added autoscroll and started making website responsive

---
+++ main.js
@@ -0,0 +1,21 @@
+const projectsLink = document.querySelector('#projects_page');
+const projectsPage = document.querySelector('#projects_section');
+const toolsLink = document.querySelector('#tools_page');
+const toolsPage = document.querySelector('#tools_section');
+const aboutLink = document.querySelector('#about_page');
+const aboutPage = document.querySelector('#about_section')
+
+projectsLink.addEventListener('click', ()=>{
+        event.preventDefault();
+        projectsPage.scrollIntoView({behavior: 'smooth', block: 'center'});
+})
+
+toolsLink.addEventListener('click', ()=>{
+        event.preventDefault();
+        toolsPage.scrollIntoView({behavior: 'smooth', block: 'center'});
+})
+
+aboutLink.addEventListener('click', ()=>{
+        event.preventDefault();
+        aboutPage.scrollIntoView({behavior: 'smooth', block: 'center'});
+})
