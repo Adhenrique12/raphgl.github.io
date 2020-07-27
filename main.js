46f07eb227f853a0444c2f0c0807c76afe2e8b8d
Rafael Lopes <rafalopes@live.com>
2020-07-27 20:56:38 -0100

    Added hamburger menu

--- main.js
+++ main.js
@@ -4,6 +4,8 @@ const toolsLink = document.querySelector('#tools_page');
 const toolsPage = document.querySelector('#tools_section');
 const aboutLink = document.querySelector('#about_page');
 const aboutPage = document.querySelector('#about_section')
+const hamburgerIcon = document.querySelector('.hamburger-menu');
+const hamburgerMenu = document.querySelector('nav ul');
 
 projectsLink.addEventListener('click', ()=>{
         event.preventDefault();
@@ -18,4 +20,9 @@ toolsLink.addEventListener('click', ()=>{
 aboutLink.addEventListener('click', ()=>{
         event.preventDefault();
         aboutPage.scrollIntoView({behavior: 'smooth'});
+})
+
+hamburgerIcon.addEventListener('click', ()=>{
+        hamburgerIcon.classList.toggle('toggle-hamburger');
+        hamburgerMenu.classList.toggle('navbar');
 })
