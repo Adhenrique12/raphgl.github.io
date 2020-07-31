193821c81e88cb3df0d8789cc131d0d9a114b7ac
Rafael Lopes <rafalopes@live.com>
2020-07-31 08:10:37 -0100

    First commit

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+// Query for tags
 const projectsLink = document.querySelector('#projects_page');
 const projectsPage = document.querySelector('#projects_section');
 const toolsLink = document.querySelector('#tools_page');
@@ -7,22 +8,37 @@ const aboutPage = document.querySelector('#about_section')
 const hamburgerIcon = document.querySelector('.hamburger-menu');
 const hamburgerMenu = document.querySelector('nav ul');
 
-projectsLink.addEventListener('click', ()=>{
-        event.preventDefault();
-        projectsPage.scrollIntoView({behavior: 'smooth'});
-})
+function focusOnSection(pageSection, pageTheme){
+        let theme = `focus-${pageTheme}`;
+        pageSection.setAttribute('id', theme);
+        setTimeout(()=>{
+                pageSection.removeAttribute('id');
+        }, 400);
+}
 
-toolsLink.addEventListener('click', ()=>{
-        event.preventDefault();
-        toolsPage.scrollIntoView({behavior: 'smooth'});
-})
+function scrollToSection(pageLink, pageSection, pageTheme){
+        pageLink.addEventListener('click', (event)=>{
+                event.preventDefault();
+                pageSection.scrollIntoView({behavior: 'smooth'});
+                if (window.innerWidth > 767){
+                        focusOnSection(pageSection, pageTheme);
+                }
+                if (window.innerWidth <= 767){
+                        toggleHamburgerMenu();
+                }
+        });
+}
 
-aboutLink.addEventListener('click', ()=>{
-        event.preventDefault();
-        aboutPage.scrollIntoView({behavior: 'smooth'});
-})
-
-hamburgerIcon.addEventListener('click', ()=>{
+function toggleHamburgerMenu(){
         hamburgerIcon.classList.toggle('toggle-hamburger');
         hamburgerMenu.classList.toggle('navbar');
+}
+
+scrollToSection(projectsLink, projectsPage, pageTheme='dark');
+scrollToSection(toolsLink, toolsPage, pageTheme='light');
+scrollToSection(aboutLink, aboutPage, pageTheme='dark');
+
+// Activate Hamburger menu when clicked
+hamburgerIcon.addEventListener('click', ()=>{
+        toggleHamburgerMenu();
 })
